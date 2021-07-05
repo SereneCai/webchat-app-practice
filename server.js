8d5e9c00d5427a581bd4d34000757daa6f91c227
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-07-05 16:28:35 +0800

    added socket.io

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app); //to use socket.io, we need access to it directly, and thus .createServer 
+const io = socketio(server); 
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//run when client connects
+io.on('connection', socket =>{
+    console.log("new connection");
+})
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log("server running"));

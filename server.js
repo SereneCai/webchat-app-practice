5a767d89593c80d79756d4003aaa09d5e84c36fa
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-07-05 17:32:27 +0800

    rectify chatmessage function

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
+const formatMsg = require('./utils/message');
+const chatRoomName = "ChatRoom";
 
 const app = express();
 const server = http.createServer(app); //to use socket.io, we need access to it directly, and thus .createServer 
@@ -12,21 +14,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 //run when client connects
 io.on('connection', socket =>{
 
-    socket.emit("message", "Welcome to the chatroom"); //emit to single user connecting
+    socket.emit("message", formatMsg(chatRoomName, "Welcome to the Chatroom!")); //emit to single user connecting
 
     //when user connects
-    socket.broadcast.emit("message", "A user have connected."); //emits to all except the user connecting
+    socket.broadcast.emit("message", formatMsg(chatRoomName, "A user connected")); //emits to all except the user connecting
 
     //when user disconnects
     socket.on('disconnect', ()=>{
-        io.emit("message", "A user had left the room.")
+        io.emit("message", formatMsg(chatRoomName, "A user has left the room."))
     })
 
     //io.emit(); // to all users
 
     //listen for chat messages
     socket.on('chatMessage', (message)=>{
-        io.emit('message', message);
+        io.emit('message', formatMsg('USER', message));
     })
 })
 const PORT = process.env.PORT || 3000;

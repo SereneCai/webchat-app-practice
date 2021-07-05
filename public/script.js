30acea9b67198ac717bb88a9861a7a0067c52c3e
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-07-05 16:50:11 +0800

    logging user input msg successful

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
 const socket = io();
+const chatForm = document.getElementById('chat-form');
+socket.on("message", (message) =>{
+    console.log(message);
+})
+
+chatForm.addEventListener('submit', (event)=>{
+    event.preventDefault(); //to stop the automatic submission to a file, the default behaviour of submit
+
+    const message = event.target.elements.msg.value; //targets the input with id 'msg' and grab the value
+
+    console.log(message);
+
+})
